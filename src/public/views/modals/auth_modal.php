b8e2f21e0a29b21b15d6943c7f7f4a731c4e359b
keysheero <kbelakov88@gmail.com>
2024-12-08 15:48:42 +0500

    ...

--- src/public/views/modals/auth_modal.php
+++ src/public/views/modals/auth_modal.php
@@ -6,7 +6,7 @@
         <h2>Вход</h2>
         <form id="loginForm">
             <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Пароль" required>
+            <input type="password" name="password" placeholder="password" required>
             <button type="button" id="submitLogin">Войти</button>
         </form>
     </div>
@@ -17,12 +17,12 @@
         <span class="close">&times;</span>
         <h2>Регистрация</h2>
         <form id="registerForm">
-            <input type="text" name="name" placeholder="Имя" required>
+            <input type="text" name="name" placeholder="Name" required>
             <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Пароль" required>
+            <input type="password" name="password" placeholder="Password" required>
             <select name="role" required>
-                <option value="User">Пользователь</option>
-                <option value="Investor">Инвестор</option>
+                <option value="User">User</option>
+                <option value="Investor">Investor</option>
             </select>
             <button type="button" id="submitRegister">Зарегистрироваться</button>
         </form>
--- src/public/views/pages/services.php
+++ src/public/views/pages/services.php
@@ -8,8 +8,8 @@ include $basePath . '/src/public/views/partials/header.php'; ?>
 <link rel="stylesheet" href="/css/template.css">
 <section class="hero">
     <div class="hero-text">
-        <h2>Want to sign a contract? </h2>
-        <p>sign a contract through application below</p>
+        <h2>Want to sign a contract?</h2>
+        <p>Sign a contract through the application below</p>
         <a href="https://online-contract.kz/" target="_blank" class="cta" id="applyNowBtn">Proceed</a>
     </div>
 </section>
@@ -17,7 +17,7 @@ include $basePath . '/src/public/views/partials/header.php'; ?>
 <section class="features">
     <div class="feature">
         <h3>Click the Link</h3>
-        <p>Follow the link to contract signing page.</p>
+        <p>Follow the link to the contract signing page.</p>
     </div>
     <div class="feature">
         <h3>Log In or Register</h3>
@@ -31,11 +31,11 @@ include $basePath . '/src/public/views/partials/header.php'; ?>
 
 <section>
     <div class="advice-section">
-        <h2>Advices</h2>
+        <h2>Advice</h2>
         <ul>
             <li><img src="/assets/img/service/contract.png" alt="Icon" class="icon"> Keep a copy of the contract</li>
             <li><img src="/assets/img/service/defining.png" alt="Icon" class="icon"> Clearly define your goals</li>
-            <li><img src="/assets/img/service/vulnerability.png" alt="Icon" class="icon"> Check for hidden clauses:</li>
+            <li><img src="/assets/img/service/vulnerability.png" alt="Icon" class="icon"> Check for hidden clauses</li>
         </ul>
     </div>
 </section>
