ca300b4be6a0cbaee2aeb546c27f7f2b349c3a98
keysheero <kbelakov88@gmail.com>
2024-12-20 20:02:21 +0500

    final version

--- src/public/views/partials/header.php
+++ src/public/views/partials/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? 'YourLib'; ?></title>
     <link rel="stylesheet" href="/css/partials/header.css">
-</head>
+</head> 
 <body>
 <header>
     <div class="container">
